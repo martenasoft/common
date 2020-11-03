dd7d810750887f11d8b5b670d4ef4bfa17e67523
primawebsystems <kostiaGm@gmail.com>
2020-11-03 12:29:07 +0200

    Added Common as bundle, added common views: from submit button, flash message

---
+++ src/Library/CommonValues.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MartenaSoft\Common\Library;
+
+interface CommonValues
+{
+    public const ADD_FLASH_SUCCESS_TYPE = 'success';
+    public const ADD_FLASH_ERROR_TYPE = 'error';
+    public const ERROR_FORM_SAVE_LOGGER_MESSAGE = 'form error save';
+}
