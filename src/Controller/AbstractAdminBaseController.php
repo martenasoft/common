23271cddf208ff855c2a620c2e2af6793aa2ceea
kanisimov <kanisimov@xmlshop.com>
2020-11-03 20:28:18 +0200

    Added new common abstract admin controller class

---
+++ src/Controller/AbstractAdminBaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MartenaSoft\Common\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use MartenaSoft\Common\Entity\BaseEntityInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+abstract class AbstractAdminBaseController extends AbstractController
+{
+
+}
--- src/Library/CommonValues.php
+++ src/Library/CommonValues.php
@@ -4,6 +4,9 @@ namespace MartenaSoft\Common\Library;
 
 interface CommonValues
 {
+    public const SITE_PAGINATION_LIMIT = 10;
+    public const ADMIN_PAGINATION_LIMIT = 10;
+
     public const ADD_FLASH_SUCCESS_TYPE = 'success';
     public const ADD_FLASH_ERROR_TYPE = 'error';
     public const ERROR_FORM_SAVE_LOGGER_MESSAGE = 'form error save';
