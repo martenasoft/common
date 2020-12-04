cb6bfb946b3bcff6785b764bc8e355bc1761af2f
kanisimov <kanisimov@xmlshop.com>
2020-12-04 20:06:33 +0200

    Content in main page

---
+++ src/Repository/AbstractCommonRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MartenaSoft\Common\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use MartenaSoft\Common\Library\CommonStatusInterface;
+
+abstract class AbstractCommonRepository extends ServiceEntityRepository
+{
+    abstract public static function getAlias(): string;
+
+    public function getQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder(static::getAlias());
+    }
+
+    protected function getStatus(): int
+    {
+        return CommonStatusInterface::STATUS_ACTIVE;
+    }
+}
